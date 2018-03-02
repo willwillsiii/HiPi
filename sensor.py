e8399e1af7c94f9059e81694401e6832b01da109
Will <whwthree@gmail.com>
2018-03-02 00:15:50 +0000

    Browser functionality added

--- sensor.py
+++ sensor.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 import RPi.GPIO as GPIO
+from tts_player import make_tts
 from pygame import mixer
 
 GPIO.setmode(GPIO.BOARD)
 mixer.init()
-mixer.music.load("lines/tmp.mp3")
+mixer.music.load(make_tts("I'm Motion Detected!"))
 try:
     GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     print("Ready!")
@@ -12,6 +13,8 @@ try:
         GPIO.wait_for_edge(16, GPIO.RISING)
         mixer.music.play()
         print("Movement Detected!")
+        GPIO.wait_for_edge(16, GPIO.FALLING)
+        print("Ready!")
 except KeyboardInterrupt:
     print("\nCloing ...")
 finally:
